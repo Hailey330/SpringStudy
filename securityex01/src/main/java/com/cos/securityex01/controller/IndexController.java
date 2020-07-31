235daf9e45699032eead1bf9c4ce644ed6b8fd3a
baegopa <baegopa@wamwam.com>
2020-07-31 14:41:27 +0900

    security - PrincipalDetails

--- securityex01/src/main/java/com/cos/securityex01/controller/IndexController.java
+++ securityex01/src/main/java/com/cos/securityex01/controller/IndexController.java
@@ -1,44 +1,57 @@
 package com.cos.securityex01.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cos.securityex01.model.User;
+import com.cos.securityex01.repository.UserRepository;
 
 @Controller
 public class IndexController {
 
-   @GetMapping({"","/"})
-   public @ResponseBody String index() {
-      return "인덱스 페이지입니다.";
-   }
-   
-   @GetMapping("/user")
-   public @ResponseBody String user() {
-      return "유저 페이지입니다.";
-   }
-   
-   @GetMapping("/admin")
-   public @ResponseBody String admin() {
-      return "관리자 페이지입니다.";
-   }
-   
-   @GetMapping("/login")
-   public String login() {
-      return "login";
-   }
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
+	private UserRepository UserRepository; // DI - 메모리에 뜸
+	
+	@GetMapping({ "", "/" })
+	public @ResponseBody String index() {
+		return "인덱스 페이지입니다.";
+	}
 
-   @GetMapping("/join")
-   public String join() {
-	   return "join";
-   }
-   
-   @PostMapping("/joinProc")
-   public String joinProc(User user) {
-	   System.out.println("회원가입 진행: " + user);
-	   return "redirect:/";
-   }
-   
+	@GetMapping("/user")
+	public @ResponseBody String user() {
+		return "유저 페이지입니다.";
+	}
+
+	@GetMapping("/admin")
+	public @ResponseBody String admin() {
+		return "관리자 페이지입니다.";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping("/join")
+	public String join() {
+		return "join";
+	}
+
+	@PostMapping("/joinProc")
+	public String joinProc(User user) {
+		System.out.println("회원가입 진행: " + user);
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+		user.setPassword(encPassword);
+		UserRepository.save(user);
+		return "redirect:/";
+	}
+
 }
--- securityex01/src/main/java/com/cos/securityex01/model/User.java
+++ securityex01/src/main/java/com/cos/securityex01/model/User.java
@@ -1,11 +1,27 @@
 package com.cos.securityex01.model;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+
 import lombok.Data;
 
+// ORM(Object Relation Mapping) 기술 : JPA
+
 @Data
+@Entity // JPA가 밑의 속성을 토대로 테이블을 생성해주는 어노테이션
 public class User {
-	private int id;
+	@Id // Primary Key를 걸어줌
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id; 
 	private String username;
 	private String password;
 	private String email;
+	@CreationTimestamp
+	private Timestamp createDate;
 }
